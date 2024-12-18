e6c60d7cdcdcc0a48a157f5f74c11b563370978b
Monjit <133980561+zephyrs12@users.noreply.github.com>
2024-12-18 11:59:26 +0530

    Update index.js (bcryptjs)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const http =require("http");
